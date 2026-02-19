cdeb015e41600e6b28586eb9bc26ab1a97a3f77d
Vladmir Boniface <47holter@gmail.com>
2026-02-19 20:24:01 +0300

    Add Firebase constants for collections

---
+++ lib/core/constants/firebase_const.dart
@@ -0,0 +1,6 @@
+class FirebaseConst {
+  static const String usersCollection = 'users';
+  static const String chatsCollection = 'chats';
+  static const String messagesCollection = 'messages';
+  static const String notificationsCollection = 'notifications';
+}
